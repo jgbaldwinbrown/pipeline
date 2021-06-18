94f0c7ccbd082fed6aa7651a4b0e4e4b2deb90fe
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-06-18 11:36:26 -0600

    init

---
+++ pipeline.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"os/exec"
+	"os"
+	"io"
+)
+
+type Pipeline_entry struct {
+	Cmd *exec.Cmd;
+	Pipe_reader *io.PipeReader;
+	Pipe_writer *io.PipeWriter;
+}
+
+type Pipeline []Pipeline_entry
+
+func Make_pipeline(cmds [][]string) Pipeline {
+	out := make(Pipeline, 0)
+	for _, e := range cmds {
+		out = append(out, Pipeline_entry{Cmd: exec.Command(e[0], e[1:]...), Pipe_reader: nil, Pipe_writer: nil})
+	}
+	return out
+}
+
+func (p Pipeline) Run(input io.Reader, output io.Writer) {
+	for i, e := range p {
+		if i == 0 {
+			e.Cmd.Stdin = input
+		}
+		if i != len(p) - 1 {
+			r, w := io.Pipe()
+			p[i+1].Cmd.Stdin = r
+			p[i+1].Pipe_reader = r
+			p[i].Cmd.Stdout = w
+			p[i].Pipe_writer = w
+		}
+		if i == len(p) - 1 {
+			e.Cmd.Stdout = output
+		}
+	}
+	for _, e := range p {
+		e.Cmd.Start()
+	}
+	for _, e := range p {
+		e.Cmd.Wait()
+		if e.Pipe_reader != nil {
+			e.Pipe_reader.Close()
+		}
+		if e.Pipe_writer != nil {
+			e.Pipe_writer.Close()
+		}
+	}
+}
+
+func main() {
+	p := Make_pipeline([][]string {
+		[]string{"echo", "-e", "apple\n", "banana\n", "carrot\n"},
+		[]string{"grep", "banana\\|carrot"},
+	})
+	/*
+	p := Pipeline {
+		exec.Command("echo", "-e", "apple\n", "banana\n", "carrot\n"),
+		exec.Command("grep", "banana\\|carrot"),
+	}
+	*/
+	p.Run(nil, os.Stdout)
+}
